e591115066498a980731b9adf36ce291cccbb57d
skefolksy <skefolksy@hotmail.com>
2018-04-26 00:21:11 +0700

    bug fix

--- arduino/go_to_zero.ino
+++ arduino/go_to_zero.ino
@@ -1,9 +1,11 @@
 void go_to_zero() {
-  for (pos_1 = 45; pos_1 <= 90; pos_1 += 1) {
-    pos_2 = 180 - pos_1;
-    SERVO_1.write(pos_1);
-    SERVO_2.write(pos_2);
-    delay(c_speed);
+  if (pos_1 != 90 && pos_2 != 90) {
+    for (pos_1 = 45; pos_1 <= 90; pos_1 += 1) {
+      pos_2 = 180 - pos_1;
+      SERVO_1.write(pos_1);
+      SERVO_2.write(pos_2);
+      delay(c_speed);
+    }
   }
 }
 
